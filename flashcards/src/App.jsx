19805e7d96a9816f543cae6b3e7fb059ba355ead
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-28 22:42:27 -0500

    add header

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,32 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Food Items But Say It In French!</h1>
+        <h3>How much of these food terms in French do you know? Except for "baguette" and "croissant"? Let's put your knowledge on the test</h3>
+        <h4>Number of Cards: 30</h4>
+      </header>
+      
     </div>
   )
 }
